98676525594d2e4a870a44c35322d7e84d13946f
PrieureDeSion <dhruv.ilesh@gmail.com>
2017-07-29 20:04:54 +0530

    Extended 2a for all aspect ratios

--- assignment1_GrayTrans/2/code/myLinearContrastStretching.m
+++ assignment1_GrayTrans/2/code/myLinearContrastStretching.m
@@ -1,9 +1,11 @@
 function img = myLinearContrastStretching(org)
 % Contrast Enhancement using Linear Contrast Stretching
-% Maps from current histogram to extended histogram
+% Maps from current histogram to extended histogram, any aspect ratio
 
 org = im2double(org);
-org_min = min(min(org));
-org_max = max(max(org));
+% Works for any aspect ratio
+org_min = repmat(min(min(org)), size(org, 1), size(org, 2));
+org_max = repmat(max(max(org)), size(org, 1), size(org, 2));
 
-img = mat2gray((org - org_min) * 255 / (org_max - org_min));
+
+img = mat2gray((org - org_min) * 255 ./ (org_max - org_min));

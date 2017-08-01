896867b95f70f5b397604b39b34395fc0961ee10
PrieureDeSion <dhruv.ilesh@gmail.com>
2017-08-02 00:59:38 +0530

    2C Done, Kinda

--- assignment1_GrayTrans/2/code/myAHE.m
+++ assignment1_GrayTrans/2/code/myAHE.m
@@ -1,18 +1,26 @@
 tic
-windowsize = 50;
-org = imread('../data/barbara.png');
-[x, y, k] = size(org)
-img = zeros(size(org));
+windowsize = 10;
+org = imread('../data/TEM.png');
+[x, y, k] = size(org);
+orgp = zeros(size(org) + 2 * [windowsize, windowsize]);
+[xp, yp, k] = size(orgp);
+orgp(windowsize+1:xp-windowsize, windowsize+1:yp-windowsize) = org;
+img = zeros(size(orgp));
+orgp = mat2gray(orgp);
 
-for i = windowsize+1:x-windowsize
-    for j = windowsize+1:y-windowsize
-        mycdf = cumsum(imhist(org(i-windowsize:i+windowsize, j-windowsize:j+windowsize)))...
-            / numel(org(i-windowsize:i+windowsize, j-windowsize:j+windowsize)); % numel fixed at 961
-        img(i, j) = mycdf(org(i, j));
+for i = windowsize+1:xp-windowsize
+    for j = windowsize+1:yp-windowsize
+        mypdf = imhist(orgp(i-windowsize:i+windowsize, j-windowsize:j+windowsize));
+        mypdf(1) = mypdf(2); % Removing the border areas. Setting equal to second entry is by arguments of gradient and continuity 
+        mycdf = cumsum(mypdf)...
+            / max(cumsum(mypdf));
+        img(i, j) = mycdf(max(round(orgp(i, j) * 255), 1));
     end
     i
 end
 toc
+img = img(windowsize+1:xp-windowsize, windowsize+1:yp-windowsize);
+
 % 
 % tic
 % windowsize = 20;

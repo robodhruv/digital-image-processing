fdfd4c6a3a5b3b300c4d83e44f5c4ecf1265f3f8
Dhruv Ilesh Shah_150070016_ <dhruvshah@rudra.ee.iitb.ac.in>
2017-08-03 19:08:54 +0530

    AHE Benchmarks

--- assignment1_GrayTrans/2/code/AHE_benchmark.m
+++ assignment1_GrayTrans/2/code/AHE_benchmark.m
@@ -1,10 +1,14 @@
 org = imread('../data/TEM.png');
-% windowsize = [5 10:10:100 125];
-windowsize = [150 175 200];
-
+windowsize = [5 10:10:100 125];
+% windowsize = [150 175 200];
+times = zeros(size(windowsize));
+i = 0;
 for w = windowsize
-    tic
+    i = i + 1;
+	tic
 	img = myAHE_par(org, w);
-    toc
+    times(i) = toc;
 	imwrite(img, strcat('AHE_benchmark/ahe_', num2str(w), '.png'));
 end
+
+save(strcat('AHE_benchmark/ahe.mat'));
---
+++ assignment1_GrayTrans/2/code/myCLAHE_par.m
@@ -0,0 +1,26 @@
+function img = myCLAHE_par(org, windowsize, threshold)
+% Performs Contrast Limited Adaptive Histogram Equalization on the input
+% image, with the windowsize and threshold passed given by the argument passed.
+
+org = org+1; % Preserving the original blacks of the image
+[x, y, k] = size(org);
+orgp = zeros(size(org) + 2 * [windowsize, windowsize]);
+[xp, yp, k] = size(orgp);
+orgp(windowsize+1:xp-windowsize, windowsize+1:yp-windowsize) = org;
+img = zeros(size(orgp));
+orgp = mat2gray(orgp);
+
+parfor i = windowsize+1:xp-windowsize
+    for j = windowsize+1:yp-windowsize
+        mypdf = imhist(orgp(i-windowsize:i+windowsize, j-windowsize:j+windowsize));
+        mypdf = mypdf(2:end); % Removing the border areas. Setting equal to second entry is by arguments of gradient and continuity
+        num_el = max(cumsum(mypdf));
+        mypdf_clipped = min(mypdf, threshold*num_el);
+        num_el_clipped = max(cumsum(mypdf_clipped));
+        mypdf_clipped = mypdf_clipped + (num_el_clipped - num_el)/255;
+        mycdf = cumsum(mypdf_clipped)...
+            / num_el;
+        img(i, j) = mycdf(max(round(orgp(i, j) * 255), 1));
+    end
+end
+img = img(windowsize+1:xp-windowsize, windowsize+1:yp-windowsize);

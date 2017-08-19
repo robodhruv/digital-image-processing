21f4cba073d96280f89e368a001a82fa3a90549c
Bhavesh Thakkar <bhavesh2109@gmail.com>
2017-08-19 15:04:23 +0530

    Run this script on the server

--- assignment2_Filtering/1/code/myUnsharpMasking.m
+++ assignment2_Filtering/1/code/myUnsharpMasking.m
@@ -1,7 +1,7 @@
 input_img=load('../data/lionCrop.mat');
 input_img=input_img.imageOrig;
-for i = 0.001:0.0001:10
-    for j=0.005:0.005:10
+for j=0.005:0.005:10
+    for i = 0.001:0.0001:10    
 figure(1);
 log_img=imfilter(input_img,fspecial('log',[9 9],i));
 subplot(1,2,1);

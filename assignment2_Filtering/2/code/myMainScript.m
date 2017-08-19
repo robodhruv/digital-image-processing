59f72dfb17dd7b93d1e4034b25eeca735f865396
Dhruv Ilesh Shah_150070016_ <dhruvshah@rudra.ee.iitb.ac.in>
2017-08-19 18:30:53 +0530

    Server Results

--- assignment2_Filtering/2/code/myMainScript.m
+++ assignment2_Filtering/2/code/myMainScript.m
@@ -5,16 +5,18 @@ img = img.imageOrig;
 noise = randn(size(img, 1)) * 0.05 * max(max(img));
 imgn = max(0, img+noise);
 
-log = zeros([size([3:2:18], 2) size([3:2:18], 2) 2]);
-i = 1; j = 1;
-for sx = [3:2:18]
-    for sr = [3:2:18]
-        sx, sr
+sx = [3:2:18];
+sr = [3:2:18];
+logger = zeros([numel(sx) numel(sr) 2]);
+for x = 1:numel(sx)
+	for r = 1:numel(sr)
+        x, r
         tic;
-        im_new = bilinear_filter(imgn, [sx, sx, sr]);
-        log(i, j, 1) = rmsd(img, im_new);
-        log(i, j, 2) = toc;
-        j = j + 1;
-    end
-    i = i + 1;
+        im_new = bilinear_filter(imgn, [sx(x), sx(x), sr(r)]);
+        logger(x, r, 1) = rmsd(img, im_new);
+        logger(x, r, 2) = toc;
+		imwrite(im_new, strcat('benchmark/', num2str(sx(x)), '_', num2str(sr(r)), '_barbara.png'));
+	end
 end
+
+save('benchmark/workspace.mat');

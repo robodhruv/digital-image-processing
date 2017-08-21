4337c3fa13715fa7c9777c8fe043b3bf2d96b64f
Dhruv Ilesh Shah_150070016_ <dhruvshah@rudra.ee.iitb.ac.in>
2017-08-21 11:34:10 +0530

    Server Results

--- assignment2_Filtering/3/code/myMainScript.m
+++ assignment2_Filtering/3/code/myMainScript.m
@@ -5,14 +5,14 @@ img = img.imageOrig;
 noise = randn(size(img, 1)) * 0.05 * max(max(img));
 imgn = max(0, img+noise);
 
-s = [1:5:100];
+s = [60:0.2:75];
 logger = zeros([numel(s) 2]);
 parfor x = 1:numel(s)
         disp(x)
         tic;
         im_new = patchbased(imgn, s(x), 9, 25);
         logger(x, 1) = rmsd(img, im_new);
-		imwrite(im_new, strcat('benchmark/', num2str(s(x)), '_barbara.png'));
+		imwrite(mat2gray(im_new), strcat('benchmark/', num2str(s(x)), '_barbara.png'));
 end
 
 save('benchmark/workspace.mat');

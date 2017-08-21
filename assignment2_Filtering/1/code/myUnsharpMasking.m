2ecfd85ca688a1391fb4ad87de33da039524bf6c
Bhavesh Thakkar <bhavesh2109@gmail.com>
2017-08-21 20:59:12 +0530

    Final Code
    Modifications in report needed

--- assignment2_Filtering/1/code/myUnsharpMasking.m
+++ assignment2_Filtering/1/code/myUnsharpMasking.m
@@ -1,13 +1,4 @@
-input_img=load('../data/lionCrop.mat');
-input_img=input_img.imageOrig;
-for j=0.005:0.005:10
-    for i = 0.001:0.0001:10    
-figure(1);
-log_img=imfilter(input_img,fspecial('log',[9 9],i));
-subplot(1,2,1);
-imshow(myLinearContrastStretching(input_img));
-subplot(1,2,2);
-imshow(myLinearContrastStretching((input_img-j*log_img)));
-print(figure(1),'-djpeg',strcat('Sigma = ',num2str(i), 'Scaling = ',num2str(j)));
-    end
-end
+function output=myUnsharpMasking(input_img,std,scaling)
+
+log_img=imfilter(input_img,fspecial('log',[9 9],std));
+output=(input_img-scaling*log_img);

e03e619fe0bb1ec09a2f6758ad64cc0ed90f4e0c
bhavesh2109 <bhavesh2109@gmail.com>
2017-10-12 02:06:21 +0530

    Q2,Q3 report done as well

--- assignment4_SVD_FaceRecognition/2/code/myMainScript.m
+++ assignment4_SVD_FaceRecognition/2/code/myMainScript.m
@@ -1,22 +1,30 @@
-%% MyMainScript
+%% Q2. Image reconstruction using top k singular values
+% Bhavesh Thakkar, Dhanvi Sreenivasan and Dhruv Ilesh Shah
 
+%% mySVDYaleReconstructor.m
+% <include>mySVDYaleReconstructor.m</include>
 tic;
 %%
-input_img=imread('../images/yaleB04_P00A-005E-10.pgm');
-input_img=im2double(a);
+
+input_img=imread('../images/yaleB04_P00A-005E-10.pgm'); %Input image chosen arbitarily
+input_img=im2double(input_img);
 U = mySVDYaleDatabasing(); %Gives us the orthogonal matrix U
-%imshow(mySVDYaleReconstructor(input_img, U ,25 ));
-K= [2 10 20 50 75 100 125 150 175]
-%%
-figure('Position', [100, 100, 800, 800])
+K = [2 10 20 50 75 100 125 150 175];
+
+%% Plot
+
+figure('Position', [100, 100, 1200, 1200]);
+subplot (4,3,2);
+imshow(input_img);
+title('Original Image');
+
 for i=1:9
-    subplot(3,3,i);
-    subimage(mySVDYaleReconstructor(input_img, U, K(i)));
-    title(strcat('k = ', int2str(K(i))))
+    subplot(4,3,3+i);
+    imshow(mySVDYaleReconstructor(input_img, U, K(i)));
+    title(strcat('k = ', int2str(K(i))));
 end
+
+
     
-
-
-
 %%
 toc;
